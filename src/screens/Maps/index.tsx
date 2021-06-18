be68ed3eff2bdb7b9d2c1cd747e23efb59d963e9
Mohamed Gamal <mge3696@gmail.com>
2021-06-18 15:30:43 +0200

    itegrated with googleMaps with ios

--- src/screens/Maps/index.tsx
+++ src/screens/Maps/index.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 interface MapsProps {}
 
 const Maps = (props: MapsProps) => {
   return (
     <MapView
       style={styles.container}
+      provider={PROVIDER_GOOGLE}
       initialRegion={{
         latitude: 37.78825,
         longitude: -122.4324,
